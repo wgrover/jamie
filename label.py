6fa5a976e625c1ca6d21beee15524ed0c26bec8b
William H. Grover <wgrover@gmail.com>
2022-11-25 11:53:13 -0800

    functional but some alignment problems

--- label.py
+++ label.py
@@ -10,24 +10,42 @@ width = 3*dpi
 height = 2*dpi
 maxwidth = width - 2*margin
 maxheight = height - 2*margin
+bestfontsize = 1
+bestwidth = 1
+bestheight = 1
+bestmessage = ""
 
 img = Image.new('RGB', (width, height), color='white')
 imgDraw = ImageDraw.Draw(img)
 
-message = "Wire"
+message = "wire wire wire wire wire"
 
+words = message.split()
+for seppattern in range(2**(len(words)-1)):
+    testmessage = ""
+    for w, word in enumerate(words):
+        sep = " "
+        if(2**w & seppattern):
+            sep = "\n"
+        testmessage += word + sep
+    testmessage = testmessage.strip()  # get rid of terminal space
+    textWidth = 1
+    textHeight = 1
+    fontsize = 0
+    while textWidth < maxwidth and textHeight < maxheight:
+        fontsize += 1
+        textWidth, textHeight = imgDraw.textsize(testmessage, font=ImageFont.truetype("FreeSansBold.ttf", size=fontsize))
+    print(repr(testmessage), fontsize, end="")
+    if fontsize > bestfontsize:
+        bestmessage = testmessage
+        bestwidth = textWidth
+        bestheight = textHeight
+        bestfontsize = fontsize
+        print(" <--- NEW BEST")
+    else:
+        print()
 
-textWidth = 0
-textHeight = 0
-fontsize = 0
-while textWidth < maxwidth and textHeight < maxheight:
-    fontsize += 1
-    font = ImageFont.truetype("FreeSansBold.ttf", size=fontsize)
-    textWidth, textHeight = imgDraw.textsize(message, font=font)
-print("winner winner chicken dinner", textWidth, textHeight)
-
-
-
-imgDraw.text((width/2, height/2), message, font=ImageFont.truetype("FreeSansBold.ttf", size=fontsize),
-              fill=(0,0,0), align="center", anchor="mm", spacing=-30)
+print("Making label with", repr(bestmessage))
+imgDraw.text((bestwidth/2+margin, bestheight/2+margin), bestmessage, font=ImageFont.truetype("FreeSansBold.ttf",
+              size=bestfontsize), fill=(0,0,0), align="center", anchor="mm", spacing=-30)
 img.save('label.png')
