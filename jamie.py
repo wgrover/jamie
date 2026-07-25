58fca1c02d05c668d72819ee14d91cebd37b0289
William H. Grover <wgrover@gmail.com>
2026-07-25 11:34:32 -0700

    Revert "trying to get editable system install"
    This reverts commit db510e8e0075efd54f9e0d73247e92e6569f58a8.

--- jamie.py
+++ jamie.py
@@ -4,65 +4,57 @@ from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 import sys, os
 
-def main():
-    dpi = 300
-    margin = 0.1  # inches
+dpi = 300
+margin = 0.1  # inches
 
-    margin = margin * dpi  # margin in dpi
-    width = 3*dpi
-    height = 2*dpi
-    maxwidth = width - 2 * margin
-    maxheight = height - 2 * margin
-    bestfontsize = 1
-    bestwidth = 1
-    bestheight = 1
-    bestmessage = ""
+margin = margin * dpi  # margin in dpi
+width = 3*dpi
+height = 2*dpi
+maxwidth = width - 2 * margin
+maxheight = height - 2 * margin
+bestfontsize = 1
+bestwidth = 1
+bestheight = 1
+bestmessage = ""
 
-    img = Image.new('RGB', (width, height), color='white')
-    imgDraw = ImageDraw.Draw(img)
-    fontfile = str(Path(__file__).with_name("FreeSansBold.ttf"))
+img = Image.new('RGB', (width, height), color='white')
+imgDraw = ImageDraw.Draw(img)
+fontfile = str(Path(__file__).with_name("FreeSansBold.ttf"))
 
-    if len(sys.argv) > 1:
-        message = sys.argv[1]
+if len(sys.argv) > 1:
+    message = sys.argv[1]
+else:
+    message = "Depleted uranium"  # a test message
+
+words = message.split()
+for seppattern in range(2**(len(words)-1)):
+    testmessage = ""
+    for w, word in enumerate(words):
+        sep = " "
+        if(2**w & seppattern):
+            sep = "\n"
+        testmessage += word + sep
+    testmessage = testmessage.strip()  # get rid of terminal space
+    textWidth = 1
+    textHeight = 1
+    fontsize = 0
+    while textWidth < maxwidth and textHeight < maxheight:
+        fontsize += 1
+        left, top, right, bottom = imgDraw.multiline_textbbox((0,0), testmessage, font=ImageFont.truetype(fontfile, size=fontsize, layout_engine=ImageFont.Layout.BASIC))
+        textWidth, textHeight = right - left, bottom - top
+    print(repr(testmessage), fontsize, end="")
+    if fontsize > bestfontsize:
+        bestmessage = testmessage
+        bestwidth = textWidth
+        bestheight = textHeight
+        bestfontsize = fontsize
+        print(" <--- NEW BEST")
     else:
-        message = "Depleted uranium"  # a test message
+        print()
 
-    words = message.split()
-    if len(words) > 1:
-        for seppattern in range(2**(len(words)-1)):
-            testmessage = ""
-            for w, word in enumerate(words):
-                sep = " "
-                if(2**w & seppattern):
-                    sep = "\n"
-                testmessage += word + sep
-            testmessage = testmessage.strip()  # get rid of terminal space
-            textWidth = 1
-            textHeight = 1
-            fontsize = 0
-            while textWidth < maxwidth and textHeight < maxheight:
-                fontsize += 1
-                left, top, right, bottom = imgDraw.multiline_textbbox((0,0), testmessage, font=ImageFont.truetype(fontfile, size=fontsize, layout_engine=ImageFont.Layout.BASIC))
-                textWidth, textHeight = right - left, bottom - top
-            # print(repr(testmessage), fontsize, end="")
-            if fontsize > bestfontsize:
-                bestmessage = testmessage
-                bestwidth = textWidth
-                bestheight = textHeight
-                bestfontsize = fontsize
-                # print(" <--- NEW BEST")
-            # else:
-            #     print()
-    else:
-        bestmessage = message
-
-
-    # print("Making label with", repr(bestmessage))
-    imgDraw.multiline_text((width/2, height/2), bestmessage, font=ImageFont.truetype(fontfile,
-                  size=bestfontsize, layout_engine=ImageFont.Layout.BASIC), fill=(0,0,0), align="center", anchor="mm")
-    img.save('label.png')
-    if len(sys.argv) > 1:  # don't print the test message
-        os.system("lpr -P _2x3_label_printer -o media=Custom.3x2in label.png")
-
-if __name__ == "__main__":
-    main()
+print("Making label with", repr(bestmessage))
+imgDraw.multiline_text((width/2, height/2), bestmessage, font=ImageFont.truetype(fontfile,
+              size=bestfontsize, layout_engine=ImageFont.Layout.BASIC), fill=(0,0,0), align="center", anchor="mm")
+img.save('label.png')
+if len(sys.argv) > 1:  # don't print the test message
+    os.system("lpr -P _2x3_label_printer -o media=Custom.3x2in label.png")
